f352c53b7cccd8e7ba37a9af1c9f5f1f9a5c3c2f
Raj Patel <rajpatel031798@gmail.com>
2020-06-14 19:19:46 -0400

    javascript folder

---
+++ JS/javascriptfile.js
@@ -0,0 +1,19 @@
+$("#carouselButton").click(function(){
+    if ($("#carouselButton").children("span").hasClass('fa-pause')) {
+        $("#mycarousel").carousel('pause');
+        $("#carouselButton").children("span").removeClass('fa-pause');
+        $("#carouselButton").children("span").addClass('fa-play');
+    }
+    else if ($("#carouselButton").children("span").hasClass('fa-play')){
+        $("#mycarousel").carousel('cycle');
+        $("#carouselButton").children("span").removeClass('fa-play');
+        $("#carouselButton").children("span").addClass('fa-pause');                    
+    }
+    
+});
+$("#ReserveButton").click(function(){
+    $('#ReserveTableModel').modal('show');
+});
+$("#LoginButton").click(function(){
+    $('#LoginModel').modal('show');
+});
